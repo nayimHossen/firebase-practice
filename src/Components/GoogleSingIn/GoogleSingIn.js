17921144ca349eadd6b18e3b61d8396134a19c85
Nayim hossen <nayimh2015@gmail.com>
2022-04-12 07:55:47 +0600

    google sign in style

--- src/Components/GoogleSingIn/GoogleSingIn.js
+++ src/Components/GoogleSingIn/GoogleSingIn.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import app from '../../firebase.inti';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
-
+import './GoogleSingIn.css';
 
 const auth = getAuth(app);
 
 
 const GoogleSingIn = () => {
+    const [user, setUser] = useState({});
     const provider = new GoogleAuthProvider();
 
     const handleSingIn = () => {
         signInWithPopup(auth, provider)
             .then(result => {
                 const user = result.user;
-                console.log(user);
+                setUser(user)
             })
             .catch(error => {
                 console.log(error);
             })
     };
 
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser({});
+            })
+            .catch(error => {
+                setUser({});
+            })
+    }
+
     return (
         <div>
-            <button onClick={handleSingIn}>Google Sing In</button>
+            {user.email ?
+                <button onClick={handleSignOut}>Sing out</button>
+                :
+                <button onClick={handleSingIn}>Google Sing In</button>}
+
+            <div className='user-cart'>
+                <img src={user.photoURL} alt="" />
+                <h2>{user.displayName}</h2>
+                <p>{user.email}</p>
+            </div>
         </div>
     );
 };
